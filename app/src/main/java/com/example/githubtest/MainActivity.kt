61506a3de69bfe9207929bffaf44872e420385d8
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-09-23 23:54:11 +0200

    New Feature

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is My Second Commit")
+        println("Another Feature on Second Branch")
     }
 }
