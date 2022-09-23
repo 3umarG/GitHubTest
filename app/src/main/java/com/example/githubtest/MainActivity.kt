a58db567855a5ba98f50e9a66af54e15d6cfec3e
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-09-23 21:49:08 +0200

    Third Commit

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is My Second Commit")
+        println("This is My Third  Commit")
     }
 }
