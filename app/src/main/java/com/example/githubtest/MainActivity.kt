09c709da82f8bc531eb4e69bde0be216f5e05a0d
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-09-24 00:28:53 +0200

    First Branch Final Commit

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("This is First Commit on First Branch")
-
         println("The Last Feature ")
     }
 }
